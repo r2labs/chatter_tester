1c7ac63b65db7c10550a61c56ffac28e49b934fe
Hershal Bhave <hershal.bhave@gmail.com>
2015-11-09 13:51:50 -0600

    more of hershals changes?

--- src/chatter_driver.cpp
+++ src/chatter_driver.cpp
@@ -1,5 +1,6 @@
 #include "chatter_driver.hpp"
 #include "std_msgs/String.h"
+#include "srv/user_input.h"
 #include "math.h"
 #include <sstream>
 #include <trajectory_msgs/JointTrajectory.h>
@@ -11,12 +12,8 @@
 void chatter_driver::spin() {
     ROS_INFO("HERSHAL: chatter spinning...");
 
-
     ros::Publisher chatter_pub =
         nh.advertise<trajectory_msgs::JointTrajectory>("tm4c/command", 1000);
-
-    // ros::Publisher chatter_pub =
-    //     nh.advertise<std_msgs::String>("tm4c/command", 1000);
 
     ros::Rate loop_rate(10);
 
@@ -38,28 +35,115 @@ void chatter_driver::spin() {
     traj.points.push_back(p);
     traj.header.stamp = ros::Time::now();
 
-    unsigned int cnt = 0;
+    float bas_us, shl_us, elb_us, wri_us, x, y, z, ga;
     while (ros::ok) {
-        std::stringstream ss;
-        ss << fgt.chat() << " " << cnt;
+
+        get_coords(x, y, z, ga);
+        set_arm(x, y, z, ga, bas_us, shl_us, elb_us, wri_us);
         
-        // std_msgs::String ros_msg;
-        // ros_msg.data = ss.str();
-        // chatter_pub.publish(ros_msg);
-        // ROS_INFO("HERSHAL: chatter_driver chatting...");
+        traj.points[0].positions[0] = bas_us;
+        traj.points[0].positions[1] = shl_us;
+        traj.points[0].positions[2] = elb_us;
+        traj.points[0].positions[3] = wri_us;
 
         chatter_pub.publish(traj);
-
         ros::spinOnce();
         loop_rate.sleep();
-        ++cnt;
     }
 }
+
+/* TODO: get coordinates from python website */
+void chatter_driver::get_coords(const srv::user_input::ConstPtr& msg) {
+    x = msg->pick_X;
+    y = msg->pick_Y;
+    z = msg->pick_Z;
+    ga = 0.0;
+    
+    /*TODO: FIGURE OUT WHERE TO STORE PLACE YOURSELF :)*/
+}
+
+int chatter_driver::set_arm(float x, float y, float z, float grip_angle_degrees,
+                             float& bas_us, float& shl_us, float& elb_us,
+                             float& wri_us) {
+
+    float grip_angle_r = radians(grip_angle_degrees);
+    float base_angle_r = atan2(y, -x);
+    float r = sqrt(pow(x,2) + pow(y,2));
+    float z_prime = z - BASE_HGT - (sin(grip_angle_r)*GRIPPER);
+    float r_prime = r - (cos(grip_angle_r)*GRIPPER);
+    float q = sqrt(pow(r_prime,2) + pow(z_prime,2));
+    if(y>=0)
+    {
+        float shoulder_angle_r = atan2(z_prime, r_prime) + acos((pow(HUMERUS,2)+pow(q,2)-pow(ULNA,2))/(2*HUMERUS*q));
+    }
+    else
+    {
+        float shoulder_angle_r = atan2(z_prime, -r_prime) + acos((pow(HUMERUS,2)+pow(q,2)-pow(ULNA,2))/(2*HUMERUS*q));
+    }
+    if (isnan(shoulder_angle_r) || isinf(shoulder_angle_r))
+    {
+        return IK_ERROR;
+    }
+
+    
+    float elbow_angle_r = acos((pow(HUMERUS,2)+pow(ULNA,2)-pow(q,2))/(2*HUMERUS*ULNA));
+    float wrist_angle_r = (3*M_PI/2) - elbow_angle_r - shoulder_angle_r + grip_angle_r;
+    
+    shl_angle_d = degrees(shoulder_angle_r);
+    elb_angle_d = degrees(elbow_angle_r);
+    wri_angle_d = degrees(wrist_angle_r);
+
+
+    // Calculate servo angles
+    // Calc relative to servo midpoint to allow compensation for servo alignment
+    float bas_pos = BAS_MID + degrees(bas_angle_r);
+    float shl_pos = SHL_MID + (shl_angle_d - 90.0);
+    float elb_pos = ELB_MID - (elb_angle_d - 90.0);
+    float wri_pos = WRI_MID + wri_angle_d;
+
+    // If any servo ranges are exceeded, return an error
+    if (bas_pos < BAS_MIN || bas_pos > BAS_MAX || shl_pos < SHL_MIN || shl_pos > SHL_MAX || elb_pos < ELB_MIN || elb_pos > ELB_MAX || wri_pos < WRI_MIN || wri_pos > WRI_MAX)
+        return IK_ERROR;
+
+    //TODO: This block should call a function that sends microseconds to TM4C
+    // bas_us = lerp(bas_pos, 0, 180, SERVO_MIN_US, SERVO_MAX_US);
+    // shl_us = lerp(shl_pos, 0, 180, SERVO_MIN_US, SERVO_MAX_US);
+    // elb_us = lerp(elb_pos, 0, 180, SERVO_MIN_US, SERVO_MAX_US);
+    // wri_us = lerp(wri_pos, 0, 180, SERVO_MIN_US, SERVO_MAX_US);
+    bas_us = radians(bas_pos);
+    shl_us = radians(shl_pos);
+    elb_us = radians(elb_pos);
+    wri_us = radians(wri_pos);
+    
+    
+    
+    //TODO: ADD GRIP CLOSE/OPEN
+}
+
+float chatter_driver::lerp(float x, float x_min, float x_max, float y_min, float y_max) {
+    if (x > x_max) { return y_max; }
+    else if (x < x_min) { return y_min; }
+    /* TODO: send information to the IFC */
+    return y_min + (y_min - y_max)*((x - x_min)/(x_max - x_min));
+}
+
+void chatter_driver::grip_close()
+{
+    //TODO: add command to close grip
+}
+
+void chatter_driver::grip_open()
+{
+    //TODO: add command to open grip
+}
+
+
+
 
 // void chatter_driver::parse_joints() {
 
 //     ros::NodeHandle priv_nh( "~" );
-    
+
 //     // Parse joints ros param
 //     XmlRpc::XmlRpcValue joints_list;
 //     if(priv_nh.getParam("joints", joints_list)) {
