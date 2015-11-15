406f93ae02e6da552ec8c531d4d62184b5d91e83
William Mauldin <stormymauldin@utexas.edu>
2015-11-15 11:44:48 -0600

    Removed profanity

--- src/chatter.cpp
+++ src/chatter.cpp
@@ -2,7 +2,7 @@
 #include <ros/ros.h> 
 
 chatter::chatter() {
-     msg = "I'm chatting, bitch!";
+     msg = "I'm chatting, fam!";
  }
 std::string chatter::chat() {
     // ROS_INFO("HERSHAL: chatter chatting...");
